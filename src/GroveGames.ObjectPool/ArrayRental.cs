0d0f9f5e62659273b57ee15a90498cbee2e082a6
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-06-13 17:18:33 +0300

    Update ArrayRental.cs

--- src/GroveGames.ObjectPool/ArrayRental.cs
+++ src/GroveGames.ObjectPool/ArrayRental.cs
@@ -1,6 +1,6 @@
 namespace GroveGames.ObjectPool;
 
-public readonly ref struct ArrayRental<T>(IArrayPool<T> pool, T[] array, bool clearArray) where T : class
+public readonly ref struct ArrayRental<T>(IArrayPool<T> pool, T[] array, bool clearArray)
 {
     private readonly IArrayPool<T> _pool = pool;
     private readonly T[] _array = array;
