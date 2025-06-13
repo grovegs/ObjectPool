1e093db7c911ba4a6db1ad2f9704fa624d540711
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-06-13 17:10:55 +0300

    feat: add array rental

---
+++ src/GroveGames.ObjectPool/ArrayPoolExtensions.cs
@@ -0,0 +1,10 @@
+namespace GroveGames.ObjectPool;
+
+public static class ArrayPoolExtensions
+{
+    public static ArrayRental<T> Rent<T>(this IArrayPool<T> pool, int size, out T[] array, bool clearArray = false) where T : class
+    {
+        array = pool.Rent(size);
+        return new ArrayRental<T>(pool, array, clearArray);
+    }
+}
---
+++ src/GroveGames.ObjectPool/ArrayRental.cs
@@ -0,0 +1,15 @@
+namespace GroveGames.ObjectPool;
+
+public readonly ref struct ArrayRental<T>(IArrayPool<T> pool, T[] array, bool clearArray) where T : class
+{
+    private readonly IArrayPool<T> _pool = pool;
+    private readonly T[] _array = array;
+    private readonly bool _clearArray = clearArray;
+
+    public T[] Value => _array;
+
+    public void Dispose()
+    {
+        _pool.Return(_array, _clearArray);
+    }
+}
