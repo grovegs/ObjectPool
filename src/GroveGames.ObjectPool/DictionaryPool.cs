d3c5d46cab11bc45c90d5df1f460aa02608105a1
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-06-13 18:18:26 +0300

    Create DictionaryPool.cs

---
+++ src/GroveGames.ObjectPool/DictionaryPool.cs
@@ -0,0 +1,51 @@
+namespace GroveGames.ObjectPool;
+
+public sealed class DictionaryPool<TKey, TValue> : IDictionaryPool<TKey, TValue> where TKey : notnull
+{
+    private readonly ObjectPool<Dictionary<TKey, TValue>> _pool;
+    private bool _disposed;
+
+    public int Count => _disposed ? throw new ObjectDisposedException(nameof(DictionaryPool<TKey, TValue>)) : _pool.Count;
+    public int MaxSize => _disposed ? throw new ObjectDisposedException(nameof(DictionaryPool<TKey, TValue>)) : _pool.MaxSize;
+
+    public DictionaryPool(int maxSize, int capacity)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(maxSize);
+        ArgumentOutOfRangeException.ThrowIfNegative(capacity);
+
+        _pool = new ObjectPool<Dictionary<TKey, TValue>>(() => new Dictionary<TKey, TValue>(capacity), static dictionary => dictionary.Clear(), maxSize);
+        _disposed = false;
+    }
+
+    public Dictionary<TKey, TValue> Rent()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        return _pool.Rent();
+    }
+
+    public void Return(Dictionary<TKey, TValue> dictionary)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        _pool.Return(dictionary);
+    }
+
+    public void Clear()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        _pool.Clear();
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+        _pool.Dispose();
+    }
+}
