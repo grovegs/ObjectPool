f9ebf8bd66d7bd83e6dae6378cd5754e35003631
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-06-14 12:12:18 +0300

    Create MultiTypeObjectPoolBuilder.cs

---
+++ src/GroveGames.ObjectPool/MultiTypeObjectPoolBuilder.cs
@@ -0,0 +1,34 @@
+using System.Collections.Frozen;
+using System.Runtime.InteropServices;
+
+namespace GroveGames.ObjectPool;
+
+public sealed class MultiTypeObjectPoolBuilder<TBase> where TBase : class
+{
+    private readonly Dictionary<Type, IObjectPool<TBase>> _poolsByType;
+
+    public MultiTypeObjectPoolBuilder()
+    {
+        _poolsByType = [];
+    }
+
+    public MultiTypeObjectPoolBuilder<TBase> AddPool<TDerived>(Func<TDerived> factory, Action<TDerived>? onReturn, int maxSize) where TDerived : class, TBase
+    {
+        var pool = new TypedObjectPool<TBase, TDerived>(factory, onReturn, maxSize);
+        var type = typeof(TDerived);
+        ref var location = ref CollectionsMarshal.GetValueRefOrAddDefault(_poolsByType, type, out bool exists);
+
+        if (exists)
+        {
+            throw new ArgumentException($"Pool for type {type.Name} has already been added.");
+        }
+
+        location = pool;
+        return this;
+    }
+
+    public FrozenDictionary<Type, IObjectPool<TBase>> Build()
+    {
+        return _poolsByType.ToFrozenDictionary();
+    }
+}
