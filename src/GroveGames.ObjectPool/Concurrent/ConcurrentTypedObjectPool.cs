d4299e0731a334d714142e671f3b068c7e64d45e
Onur Tanrıkulu <onurtnrkl@gmail.com>
2025-06-14 14:46:10 +0300

    Create ConcurrentTypedObjectPool.cs

---
+++ src/GroveGames.ObjectPool/Concurrent/ConcurrentTypedObjectPool.cs
@@ -0,0 +1,54 @@
+namespace GroveGames.ObjectPool.Concurrent;
+
+public sealed class ConcurrentTypedObjectPool<TBase, TDerived> : IObjectPool<TBase> where TDerived : class, TBase where TBase : class
+{
+    private readonly ConcurrentObjectPool<TDerived> _pool;
+    private volatile bool _disposed;
+
+    public int Count => _disposed ? throw new ObjectDisposedException(nameof(ConcurrentTypedObjectPool<TBase, TDerived>)) : _pool.Count;
+    public int MaxSize => _disposed ? throw new ObjectDisposedException(nameof(ConcurrentTypedObjectPool<TBase, TDerived>)) : _pool.MaxSize;
+
+    public ConcurrentTypedObjectPool(Func<TDerived> factory, Action<TDerived>? onRent, Action<TDerived>? onReturn, int initialSize, int maxSize)
+    {
+        ArgumentNullException.ThrowIfNull(factory);
+        ArgumentOutOfRangeException.ThrowIfNegative(initialSize);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(initialSize, maxSize);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(maxSize);
+
+        _pool = new ConcurrentObjectPool<TDerived>(factory, onRent, onReturn, initialSize, maxSize);
+        _disposed = false;
+    }
+
+    public TBase Rent()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        return _pool.Rent();
+    }
+
+    public void Return(TBase item)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        var derivedItem = (TDerived)item;
+        _pool.Return(derivedItem);
+    }
+
+    public void Clear()
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        _pool.Clear();
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        _disposed = true;
+        _pool.Dispose();
+    }
+}
