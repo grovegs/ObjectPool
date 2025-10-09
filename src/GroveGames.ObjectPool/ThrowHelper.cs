bb56942e60e56a2b72caa7581946da0c06044520
Onur Tanrıkulu <onurtnrkl@users.noreply.github.com>
2025-10-09 18:42:02 +0300

    ci: update workflows (#17)
    * Update ThrowHelper.cs
    
    * Update release.yml
    
    * Update format.yml
    
    * Update format.yml
    
    * Update tests.yml
    
    * Update ThrowHelper.cs

--- src/GroveGames.ObjectPool/ThrowHelper.cs
+++ src/GroveGames.ObjectPool/ThrowHelper.cs
@@ -7,41 +7,35 @@ namespace GroveGames.ObjectPool;
 internal static class ThrowHelper
 {
     [DoesNotReturn]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowNegativeValue(string paramName)
     {
         throw new ArgumentOutOfRangeException(paramName, "Value must be non-negative.");
     }
 
     [DoesNotReturn]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowGreaterThan(string paramName, int value, int max)
     {
         throw new ArgumentOutOfRangeException(paramName, value, $"Value must be less than or equal to {max}.");
     }
 
     [DoesNotReturn]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowNegativeOrZero(string paramName)
     {
         throw new ArgumentOutOfRangeException(paramName, "Value must be positive.");
     }
 
     [DoesNotReturn]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowObjectDisposed(object instance)
     {
         throw new ObjectDisposedException(instance.GetType().FullName);
     }
 
     [DoesNotReturn]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowArgumentNull(string paramName)
     {
         throw new ArgumentNullException(paramName);
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ThrowIfNegative(int value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
     {
         if (value < 0)
@@ -50,7 +44,6 @@ internal static class ThrowHelper
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ThrowIfGreaterThan(int value, int max, [CallerArgumentExpression(nameof(value))] string? paramName = null)
     {
         if (value > max)
@@ -59,7 +52,6 @@ internal static class ThrowHelper
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ThrowIfNegativeOrZero(int value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
     {
         if (value <= 0)
@@ -68,7 +60,6 @@ internal static class ThrowHelper
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ThrowIfDisposed(bool disposed, object instance)
     {
         if (disposed)
@@ -77,7 +68,6 @@ internal static class ThrowHelper
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ThrowIfNull<T>(T? value, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : class
     {
         if (value is null)
